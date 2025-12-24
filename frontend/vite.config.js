e99331509a9f9d5826f12b67cc6e00baf064dedd
Xariskaramf <>
2025-12-24 15:23:04 +0200

    Add jsconfig.json for TypeScript configuration and update vite.config.js for path aliasing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path'; 
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   server: {
+    host: true, 
+    strictPort: true,
+    port: 3000, 
     proxy: {
       '/api': {
-        target: 'http://localhost:5050',
+        target: 'http://my-app-backend:5050',
+        changeOrigin: true,
+        secure: false,
       },
     },
   },
